0a8b3818f9b9c25a1d3f53bb30ed3badca5ed299
zacharyhuguet <71440891+zacharyhuguet@users.noreply.github.com>
2021-04-26 18:02:42 +0200

    second

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-include('conf.php');
+session_start();
+include_once('connect.php');
+
 
 $bdd = mysqli_connect(BDHOST, BDUSER, BDPASSWORD, BDNAME);
 
@@ -16,6 +18,11 @@ while($donnees = mysqli_fetch_assoc($resultat)){
 }
 echo "<br/><a href='ajouter.php'>Ajouter</a>"
 */
+
+$sql = 'SELECT id, label, FROM tbl_type;';
+$result = mysqli_query($bd,$slq);
+
+include_once('close.php');
 ?>
 
 <!DOCTYPE html>
@@ -24,10 +31,31 @@ echo "<br/><a href='ajouter.php'>Ajouter</a>"
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Liste des types de livres</title>
 </head>
 <body>
 
-    
+<div class="container">
+<table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Libellé</th>
+      <th>Action</th>
+    </thead>
+<?php
+foreach($result as $type){
+?>   
+ <tr>
+ <td><?php print($type['id']); ?></td>
+ <td><?php print($type['label']); ?></td>
+ <td><a href="detailphp?id=<?php print($type['ID']); ?>">voir</a> Modifier Supprimer </td>
+ </tr>
+}
+
+
+</table>
+<div>
+
 </body>
 </html>
